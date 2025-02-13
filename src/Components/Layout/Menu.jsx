e272497027340fbbec83329370733db19dcf6fe1
XandePedrassani <alexandrecouss@hotmail.com>
2025-02-13 20:37:25 -0300

    Removendo lixos

--- src/Components/Layout/Menu.jsx
+++ src/Components/Layout/Menu.jsx
@@ -13,21 +13,6 @@ function Menu() {
                         <Link to="/login" className="nav-link">Login</Link>
                     </li>
                     <li className="nav-item">
-                        <Link to="/categorias" className="nav-link">Categoria Listar</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/nova-categoria" className="nav-link">Categoria Cadastrar</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/receitas-categoria" className="nav-link">Receita Listar por Categoria</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/receitas" className="nav-link">Receita Listar</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/nova-receita" className="nav-link">Receita Cadastrar</Link>
-                    </li>
-                    <li className="nav-item">
                         <Link to="/usuarios" className="nav-link">Usuario Listar</Link>
                     </li>
                     <li className="nav-item">
